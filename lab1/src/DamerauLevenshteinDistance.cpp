50069cc3555672332a181ea1ddce13a8bef6ba48
Volodya Larin <volodyalarin@bk.ru>
2021-10-11 00:28:30 +0300

    update

--- lab1/src/DamerauLevenshteinDistance.cpp
+++ lab1/src/DamerauLevenshteinDistance.cpp
@@ -15,18 +15,16 @@ int rec_dl(const std::string &left, const std::string &right, const int i, const
 
     int m = left[i - 1] == right[j - 1] ? 0 : 1;
 
-    std::vector<int> res = {
+    int res = std::min({
             rec_dl(left, right, i, j - 1) + 1,
             rec_dl(left, right, i - 1, j) + 1,
             rec_dl(left, right, i - 1, j - 1) + m
-    };
+    });
     if (i > 1 && j > 1 && left[i - 1] == right[j - 2] && left[i - 2] == right[j - 1]) {
-        res.push_back(
-                rec_dl(left, right, i - 2, j - 2) + 1
-        );
+        res = std::min(res, rec_dl(left, right, i - 2, j - 2) + 1);
     }
 
-    return *std::min_element(res.begin(), res.end());
+    return res;
 }
 
 int rec_dl(const std::string &left, const std::string &right) {
